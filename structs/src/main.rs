27633b6de2d581b60af0ebd6b93834b055483329
Fredpwol <fredpwol@gmail.com>
2022-01-29 23:01:37 +0100

    added base structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,24 @@
+struct Boy{
+    name: String,
+    age: i32
+}
+
+impl Boy {
+    fn shout() -> String{
+        return "woaaah!!!!!".to_string();
+    }
+}
+
+struct  GenricCar<T>{
+    speed: T,
+    inetia: T
+}
+
+impl <T> GenricCar<T>{
+    fn drive(&self, time: T) -> T{
+        time * self.speed + self.inetia
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
